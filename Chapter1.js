eee2d21da87207e35b0955f8aff9b653317d36f3
Sarim-Siraj <sarimsirajss@gmail.com>
2024-09-14 22:05:56 +0500

    N-add

---
+++ Chapter1.js
@@ -0,0 +1,11 @@
+// Question 1
+// alert("Error! Please Enter a Valid Password")
+
+// Question 2
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+// Qusetion 3
+// alert("Welcome to JS Land..")
+
+// Question 5
+// alert("Hello...I can run JS through my web browser console")
