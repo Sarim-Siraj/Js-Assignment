3b9886d9fe5a0ca69767d7c46a0ca33caa92545b
Sarim-Siraj <sarimsirajss@gmail.com>
2024-09-15 10:39:28 +0500

    update

--- chapter5.js
+++ chapter5.js
@@ -1,5 +1,49 @@
 // Question 1
-var a=5
-var b =7
+// var a=5
+// var b =7
 // var c=a+b
-document.write(a+b)
+// document.write("The sum of "+a+"and "+b+ "is "+c+"</br>")
+
+// Question 2
+// var a=5
+// var b =7
+// var c=a-b
+// document.write("The sub of "+a+"and "+b+ "is "+c+"</br>")
+
+// var a=5
+// var b =7
+// var c=a*b
+// document.write("The Multiply of "+a+"and "+b+ "is "+c+"</br>")
+
+// var a=2
+// var b =10
+// var c=a%b
+// document.write("The Modulus of "+a+"and "+b+ "is "+c+"</br>")
+
+// Question 3
+// var num1;
+// num1=" Undifine."
+// document.write("Value after variable declaration is"+num1+"</br>")
+
+
+// var num2;
+// num2 =5;
+// document.write("Initial Value: "+num2+"</br>")
+
+
+
+// ++num2;
+// document.write("Valueafter increment is: "+num2 +"</br>")
+
+// var num3=num2+7
+// document.write("Value after addition is: "+num3 +"</br>")
+
+// --num3
+// document.write("Value after decrement is: "+num3+"</br>")
+
+// var num4=num3%3
+// document.write("The remainder is:"+num4)
+
+
+
+
